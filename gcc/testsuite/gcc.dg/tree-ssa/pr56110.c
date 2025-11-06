d5e3a6a59b8081a311ade64f5fea2d9e09abd8ad
Daniel Barboza <daniel.barboza@oss.qualcomm.com>
2025-11-06 14:35:17 -0800

    tree-ssa-phiopt.cc: canonicalize_conditional_ops ()
    Add a function that handles a diamond pattern with consecutive bitops
    (bitclear and bitset) and canonicalize the then/else legs.  The result
    is a simpler format that can be optimize further.

---
+++ gcc/testsuite/gcc.dg/tree-ssa/pr56110.c
@@ -0,0 +1,44 @@
+/* { dg-additional-options -O2 } */
+/* { dg-additional-options -fdump-tree-phiopt3 } */
+
+unsigned f1 (unsigned x, unsigned m)
+{
+    if (m & 0x008080)
+        x >>= 8;
+
+    return x;
+}
+
+unsigned f2 (unsigned x, unsigned m)
+{
+    if (m & 0x008080)
+        x <<= 8;
+
+    return x;
+}
+
+unsigned f3 (unsigned x, unsigned m)
+{
+    if (m & 0x008080)
+        x |= 8;
+
+    return x;
+}
+
+unsigned f4 (unsigned x, unsigned m)
+{
+    if (m & 0x008080)
+        x = x ^ 8;
+
+    return x;
+}
+
+unsigned f5 (unsigned x, unsigned m)
+{
+    if (m & 0x008080)
+        x = x ^ 1;
+
+    return x;
+}
+
+/* { dg-final { scan-tree-dump-times "PHI" 0 phiopt3 } } */
