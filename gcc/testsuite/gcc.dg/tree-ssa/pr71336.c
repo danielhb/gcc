c720b4954e0271d18f6efcdc028c947eb9edf3b2
Daniel Barboza <daniel.barboza@oss.qualcomm.com>
2026-03-17 13:31:25 -0300

    temp

--- gcc/testsuite/gcc.dg/tree-ssa/pr71336.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr71336.c
@@ -2,19 +2,31 @@
 /* { dg-additional-options -fdump-tree-gimple } */
 
 int test (int a) {
-    return a & 1 ? 7 : 3;
+  return a & 1 ? 7 : 3;
 }
 
 int test2 (int a) {
-    return (a & 1) == 0 ? 3 : 7;
+  return (a & 1) == 0 ? 3 : 7;
 }
 
 int test3 (int a) {
-    return a & 1 ? 17 : 3;
+  return a & 1 ? 3 : 7;
 }
 
 int test4 (int a) {
-    return (a & 1) == 0 ? 3 : 17;
+  return (a & 1) == 0 ? 7 : 3;
 }
 
-/* { dg-final { scan-tree-dump-times " goto " 0 gimple } } */
+int test5 (int a) {
+  return a & 1 ? 17 : 3;
+}
+
+int test6 (int a) {
+  return (a & 1) == 0 ? 3 : 17;
+}
+/* { dg-final { scan-tree-dump-times " << 2;" 4 gimple } } */
+/* { dg-final { scan-tree-dump-times " 7 - " 2 gimple } } */
+/* { dg-final { scan-tree-dump-times " \\+ 3;" 2 gimple } } */
+/* { dg-final { scan-tree-dump-times " = 3;" 2 gimple } } */
+/* { dg-final { scan-tree-dump-times " = 17;" 2 gimple } } */
+/* { dg-final { scan-tree-dump-times " goto " 6 gimple } } */
