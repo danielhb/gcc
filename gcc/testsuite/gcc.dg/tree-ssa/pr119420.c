84ce96ecaca3d866c419477089f3c034dbc5d4a6
Daniel Barboza <daniel.barboza@oss.qualcomm.com>
2026-04-16 17:49:50 -0300

    add NE variant, clz-complement-int.c is broken

--- gcc/testsuite/gcc.dg/tree-ssa/pr119420.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr119420.c
@@ -19,5 +19,23 @@ int ll2 (signed a)
   return aa <= 1;
 }
 
+int ll3 (signed a)
+{
+  int d = a >> 1;
+  return d == 0;
+}
+
+int ll4 (signed a)
+{
+  int d = a & ~1;
+  return d == 0;
+}
+
+int ll5 (signed a)
+{
+  unsigned aa = a;
+  return aa <= 1;
+}
+
 /* { dg-final { scan-tree-dump-times " >> " 0 optimized } } */
 /* { dg-final { scan-tree-dump-times " <= " 3 optimized } } */
