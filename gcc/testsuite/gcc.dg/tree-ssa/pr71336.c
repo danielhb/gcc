e5d38113b67059c20c202ecefe4e78de86b08315
Daniel Barboza <daniel.barboza@oss.qualcomm.com>
2026-07-20 14:46:59 -0300

    convert to match.pd zero_one patterns

--- gcc/testsuite/gcc.dg/tree-ssa/pr71336.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr71336.c
@@ -9,16 +9,14 @@ int test2 (int a) {
     return (a & 1) == 0 ? 3 : 7;
 }
 
-/* Will not be simplified.  */
 int test3 (int a) {
     return a & 1 ? 17 : 3;
 }
 
-/* Will not be simplified.  */
 int test4 (int a) {
     return (a & 1) == 0 ? 3 : 17;
 }
 
-/* { dg-final { scan-tree-dump-times " << " 2 optimized } } */
-/* { dg-final { scan-tree-dump-times " goto " 4 optimized } } */
+/* { dg-final { scan-tree-dump-times " \\* " 4 optimized } } */
+/* { dg-final { scan-tree-dump-times " goto " 0 optimized } } */
  
